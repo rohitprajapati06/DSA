00941bd830bbce90463650b7f70dc88fc70b7ddb
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-06-12 14:45:09 +0530

    Basics Operations

--- Basics/Linked List/Doubly Linked List.cs
+++ Basics/Linked List/Doubly Linked List.cs
@@ -58,16 +58,68 @@ public class DoublyLinkedList{
 	   }
 	   Console.WriteLine("null");
    }
+	
+	public bool Contains(int data){
+		
+		 Node current = head;
+		while(current != null){
+			 
+			if(current.Data == data){
+				return true;
+			}
+			
+			current = current.Next;
+		}
+		return false;
+	}
+	
+	public void Delete(int data){
+		
+		Node newNode = new Node(data);
+		if(head.Data == data){
+			head = head.Next;
+			head.Prev = null;
+			return;
+		}
+		
+		Node current = head ;
+		while(current != null && current.Data != data){
+			current = current.Next;
+		}
+		 if (current.Next != null)
+            current.Next.Prev = current.Prev;
+
+        if (current.Prev != null)
+            current.Prev.Next = current.Next;
+	}
+	
+	public int Count(){
+		
+		int count = 0;
+		Node current = head;
+		while(current != null){
+			count++;
+			current = current.Next;
+		}
+		
+		return count;
+
+	}
 
 }
 
 public class Program{
 	public static void Main(string[] args){
-	DoublyLinkedList list = new DoublyLinkedList();
-	list.Append(10);
-	list.Append(20);
-	list.Append(30);
-	list.Prepend(5);
-	list.Print();
+		DoublyLinkedList list = new DoublyLinkedList();
+			list.Append(10);
+			list.Append(20);
+			list.Append(30);
+			list.Prepend(5);
+			list.Print();
+			Console.WriteLine("Linked List contains 20 : "+ list.Contains(20));
+			list.Delete(30);
+			list.Print();
+			Console.WriteLine("Total Nodes in Linked List  : "+ list.Count());
+
 	}
 }
